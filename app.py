f0e2ad828b40c40cf4e48f8bc8de63092081b6a4
Evandro Venancio <evandro.venancio@emanagecrm.com>
2020-02-25 09:32:20 -0300

    adding new environment + flask-restful

--- app.py
+++ app.py
@@ -1,59 +1,29 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-stores = [
-    {
-        'name': 'My First Store',
-        'items': [
-            { 'name': 'My item', 'price': 15.99 }
-        ]
-    }
-]
+items = []
 
-@app.route('/store', methods=['POST'])
-def create_store():
-    request_data = request.get_json()
-    new_store = {
-        'name': request_data['name'],
-        'items': []
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
+class Item(Resource):    
+    def get(self, name):
+        for item in items:
+            if(item['name'] == name):
+                return item
+        return { 'message': 'item not found' }, 404       
+    
+    def post(self, name):
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+    
+class ItemList(Resource):
+    def get(self):
+        return { 'items': items}
+    
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-        
-    return jsonify({'message':'store not found'})
-            
-
-@app.route('/store')
-def get_stores():
-    return jsonify({ 'stores': stores })
-
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if(store['name'] == name):
-            new_item = {
-                'name': request_data['name'],
-                'price': request_data['price']
-            }
-            store['items'].append(new_item)
-            return jsonify(new_item)
-        
-    return jsonify({'message':  'store not found'})
-
-@app.route('/store/<string:name>/item')
-def get_items_in_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store['items'])
-        
-    return jsonify({'message':'store not found'})
-
-app.run(port=5000);
+app.run(port=5000)
